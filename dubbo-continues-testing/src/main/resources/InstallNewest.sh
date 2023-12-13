4ebd780a48be75f914ad7b058fb765e205961c9e
林尧 <wangxiaobin@cai-inc.com>
2023-12-13 21:45:23 +0800

    use act hook

--- dubbo-continues-testing/src/main/resources/InstallNewest.sh
+++ dubbo-continues-testing/src/main/resources/InstallNewest.sh
@@ -5,9 +5,9 @@
 
 cd $1/../dubbo
 git pull
-sudo -u zcy mvn clean install -Drevision=$2 -Dmaven.test.skip=true --settings /Users/zcy/Documents/work/tool/apache-maven-3.8.5/settings.xml
+sudo -u $(whoami) mvn clean install -Drevision=$2 -Dmaven.test.skip=true --settings ~/work/settings.xml
 
 cd $1/dubbo-continues-testing-demo
-mvn clean install -Drevision=$2  --settings /Users/zcy/Documents/work/tool/apache-maven-3.8.5/settings.xml
+mvn clean install -Drevision=$2  --settings ~/work/settings.xml
 java -jar $1/dubbo-continues-testing-demo/target/testing-demo.jar
 
