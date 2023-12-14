b147971610ba5be8bb4905f86c6245800e55914e
林尧 <wangxiaobin@cai-inc.com>
2023-12-14 20:07:22 +0800

    fix

--- dubbo-continues-testing/src/main/resources/InstallNewest.sh
+++ dubbo-continues-testing/src/main/resources/InstallNewest.sh
@@ -2,7 +2,7 @@
 
 # $1: the path of dubbo-continues-testing
 # $2: the version of dubbo-continues-testing
-
+echo aaaa
 cd $1/../dubbo
 git pull
 sudo -u $(whoami) mvn clean install -Drevision=$2 -Dmaven.test.skip=true --settings ~/work/settings.xml
